f01618503a0b30d045cc9d3d7daabf0bcd905224
LUCAS LEE JING YI <lucasleeeeeeeee@gmail.com>
2020-02-26 23:39:40 +0800

    Program: a lot of changes
    functionality remains broadly similar.

--- src/main/kotlin/Program.kt
+++ src/main/kotlin/Program.kt
@@ -10,109 +10,114 @@ import kotlin.coroutines.coroutineContext
 /**
  * Main function, self explanatory.
  */
-suspend fun main(args: Array<String>) {
-    println("Hello World")
-    val db = getDBConnection()
-    if(db != null){
-        println("Connection Successful!!")
-        println(testSQLQueries(db))
-    }
-    if(db!=null) {
-        val list = ArrayList<Deferred<Unit>>()
-        runBlocking {
-            //specifically 8 because I have 32 threads, but I doubt Dispatchers.IO has that many threads in the threadpool
-            for (i in 0..8) {
-                launch(Dispatchers.IO)  {
-                    insertBullshit("19", db)
-                }
-            }
-            for (i in 0..8) {
-                launch(Dispatchers.IO)  {
-                    insertBullshit("20", db)
-                }
-            }
-            for (i in 0..8){
-                launch(Dispatchers.IO)  {
-                    insertBullshit("21", db)
-                }
-            }
-            for (i in 0..8) {
-                launch(Dispatchers.IO)  {
-                    insertBullshit("22", db)
-                }
-            }
-    }
 
-    }
-
-}
-
-/**
- * Gets the database connection, we are using JDBC.
- */
-fun getDBConnection(): Connection?{
-    val credentials = Properties()
-    credentials.put("user", "root")
-    credentials.put("password", "")
-    try{
-        var connection = DriverManager.getConnection(
-            "jdbc:mysql://127.0.0.1:3306/",
-            credentials)
-        return connection
-    }catch (ex: SQLException){
-        ex.printStackTrace()
-    }
-    return null
-}
-
-/**
- * this was just a test to try out some JDBC functions and to practice some kotlin
- */
-fun testSQLQueries(conn: Connection): String?{
-    var returnstring = ""
-    try {
-        var sql = conn.createStatement()
-        var result = sql.executeQuery("SHOW DATABASES;")
-        while (result!!.next()){
-            returnstring += (result.getString("Database")+"\n")
+object Program {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello World")
+        val db = getDBConnection()
+        if (db != null) {
+            println("Connection Successful!!")
+            println(testSQLQueries(db))
         }
-        return returnstring
-    }catch (ex: SQLException){
-        ex.printStackTrace()
-    }
-    return null
-}
+        if (db != null) {
+            val list = ArrayList<Deferred<Unit>>()
+            val nproc = Runtime.getRuntime().availableProcessors()
+            runBlocking {
+                //spawn i number of threads to insert bullshit into the database
+                //more threads = more data.
+                for (i in 0..nproc) {
+                        Thread {
+                            //16 as we start from year 16
+                            insertBullshit((i + 4) + 16, db)
+                        }.start()
+                    }
+                }
+            }
 
-/**
- * This is the fun part, we will insert dummy data into the database, randomly generating numbers like AdminNumber
- * I might make it more efficient by instead incrementing the Admin Number and increasing the packet size rather than being lazy and spawning heaps of coroutines/threads
- */
-suspend fun insertBullshit(year:String, conn: Connection){
-    //to uppercase cos im lazy as fuck
-    val alphabets = "qwertyuiopasdfghjklzxcvbnm".toUpperCase()
-    val rand = Random()
-    val query = "INSERT IGNORE INTO `covid19db`.`users` (`AdminNumber`, `PasswordSalt`, `PasswordHash`, `Firstname`, `MiddleName`, `LastName`, `Gender`, `isAdmin`, `isCurrent`) VALUES (?,?,?,?,?,?,?,?,?);"
-    val statement = conn.prepareStatement(query)
-    //10000 is just under the maximum packet size of 1GB
-    println(Thread.currentThread().name)
-    for (i in 0..10000) {
+        }
 
-        statement.setString(1, year + rand.nextInt(99999).toString() + alphabets[rand.nextInt(26)])
-        statement.setString(2, "Password")
-        statement.setString(3, "Password")
-        statement.setString(4, "First")
-        statement.setString(5, "Middle")
-        statement.setString(6, "Last")
-        statement.setInt(7, rand.nextInt(2))
-        statement.setInt(8, rand.nextInt(2))
-        statement.setInt(9, rand.nextInt(2))
+    /**
+     * Gets the database connection, we are using JDBC.
+     */
+    fun getDBConnection(): Connection? {
+        val credentials = Properties()
+        credentials.put("user", "root")
+        credentials.put("password", "")
         try {
-            statement.addBatch()
+            var connection = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/",
+                credentials
+            )
+            return connection
+        } catch (ex: SQLException) {
+            println(ex.stackTrace)
+            ex.printStackTrace()
+        }
+        return null
+    }
+
+    /**
+     * this was just a test to try out some JDBC functions and to practice some kotlin
+     */
+    fun testSQLQueries(conn: Connection): String? {
+        var returnstring = ""
+        try {
+            var sql = conn.createStatement()
+            var result = sql.executeQuery("SHOW DATABASES;")
+            while (result!!.next()) {
+                returnstring += (result.getString("Database") + "\n")
+            }
+            return returnstring
         } catch (ex: SQLException) {
             ex.printStackTrace()
         }
+        return null
     }
-    //execute all the batch statements at the end
-    statement.executeBatch()
+    fun insertRooms(){
+        val query = "INSERT INTO `covid19db`.`locations` (`RoomName`, `TierLevel`) VALUES (?,?);"
+        for(level in 1..9) {
+            for (room in 1..11) {
 
+            }
+        }
+    }
+    fun insertTravelHistory(){
+
+    }
+    /**
+     * This is the fun part, we will insert dummy data into the database, randomly generating numbers like AdminNumber
+     * I might make it more efficient by instead incrementing the Admin Number and increasing the packet size rather than being lazy and spawning heaps of coroutines/threads
+     */
+    fun insertBullshit(year: Int, conn: Connection) {
+        //to uppercase cos im lazy as fuck
+        val alphabets = "qwertyuiopasdfghjklzxcvbnm".toUpperCase()
+        val rand = Random()
+        val query =
+            "INSERT IGNORE INTO `covid19db`.`users` (`AdminNumber`, `PasswordSalt`, `PasswordHash`, `Firstname`, `MiddleName`, `LastName`, `Gender`, `isAdmin`, `isCurrent`) VALUES (?,?,?,?,?,?,?,?,?);"
+        //infinite loop until we're happy
+        while (true) {
+            val statement = conn.prepareStatement(query)
+            //10000 is just under the maximum packet size of 1GB
+            for (i in 0..10000) {
+                statement.setString(1, rand.nextInt(999999).toString() + alphabets[rand.nextInt(26)])
+                statement.setString(2, "Password")
+                statement.setString(3, "Password")
+                statement.setString(4, "First")
+                statement.setString(5, "Middle")
+                statement.setString(6, "Last")
+                statement.setInt(7, rand.nextInt(2))
+                statement.setInt(8, rand.nextInt(2))
+                statement.setInt(9, rand.nextInt(2))
+                try {
+                    statement.addBatch()
+                } catch (ex: SQLException) {
+                    ex.printStackTrace()
+                }
+            }
+            println("Inserting batch of 10000")
+            //execute all the batch statements at the end
+            statement.executeBatch()
+        }
+    }
 }
